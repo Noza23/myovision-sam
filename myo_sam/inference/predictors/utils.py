ba46ac8c7591afecfed7acab17ab2cb2a34302d4
Noza23 <G.Nozadze@campus.lmu.de>
2024-01-23 15:55:32 +0100

    moved split axis to inference + extended  merge_over_axis by ids, needs to be opt

--- myo_sam/inference/predictors/utils.py
+++ myo_sam/inference/predictors/utils.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import cv2
 import numpy as np
 
@@ -39,6 +41,45 @@ def box_in_box(box: np.ndarray, boxes: np.ndarray) -> np.ndarray:
     return np.all((box[0] >= boxes[:, 0]) & (box[-1] <= boxes[:, -1]), axis=1)
 
 
+def split_image_into_patches(
+    image: np.ndarray, patch_size: tuple[int, int]
+) -> tuple[tuple[int, int], list[np.ndarray]]:
+    """Split an image into patches of a given size."""
+    height, width = image.shape[:2]
+    patch_height, patch_width = patch_size
+
+    if patch_height > height or patch_width > width:
+        return (0, 0), [image]
+
+    patches = []
+    width_reminder = width % patch_width
+    height_reminder = height % patch_height
+    width_range = [*range(0, width, patch_width)]
+    height_range = [*range(0, height, patch_height)]
+
+    # if reminder less than quarter of patch-size, merge it to the last patch
+    if width_reminder < patch_size[0] / 4:
+        width_range[-1] += width_reminder
+    else:
+        width_range.append(width)
+    # if reminder less than quarter of patch-size, merge it to the last patch
+    if height_reminder < patch_size[1] / 4:
+        height_range[-1] += height_reminder
+    else:
+        height_range.append(height)
+
+    # grid is needed for later reconstruction
+    grid = (len(height_range) - 1, len(width_range) - 1)
+
+    for i in range(len(height_range) - 1):
+        for j in range(len(width_range) - 1):
+            left, right = width_range[j], width_range[j + 1]
+            lower, upper = height_range[i], height_range[i + 1]
+            patch = image[lower:upper, left:right, ...]
+            patches.append(patch)
+    return grid, patches
+
+
 def remove_disconnected_masks(roi_coords: np.ndarray):
     """Remove disconnected masks."""
     # Background is always connected
@@ -52,7 +93,7 @@ def merge_masks_at_splitponits(
     patch_size: tuple[int, int],
     iou_threshold: float = 0.85,
     max_offset: int = 1,
-) -> list[np.ndarray]:
+) -> tuple[list[np.ndarray], list[list]]:
     """
     Merge masks at the split points.
 
@@ -63,20 +104,23 @@ def merge_masks_at_splitponits(
 
     Returns:
         list of roi_coords merged at the split points.
+        list of list of ids containing the ids of the merged masks.
     """
+    ids = np.arange(len(roi_coords))[:, None].tolist()
     if grid[1] > 1:
         axis = 0
         edges_x = np.arange(0, grid[1] * patch_size[1], patch_size[1])[1:]
         for edge in edges_x:
             edges = np.arange(edge - max_offset, edge + max_offset + 1)
             is_on_edge_x = [is_on_edge(roi, edges, axis) for roi in roi_coords]
-            edge_x_insts = [
-                roi for i, roi in zip(is_on_edge_x, roi_coords) if i
-            ]
-            edge_x_merged = merge_over_axis(
-                edge_x_insts, axis, edges, iou_threshold
+            ids_on_edge_x = [id_x for i, id_x in zip(is_on_edge_x, ids) if i]
+            ids = [id_x for i, id_x in zip(is_on_edge_x, ids) if not i]
+            edge_x_insts = [ro for i, ro in zip(is_on_edge_x, roi_coords) if i]
+            edge_x_merged, merged_ids = merge_over_axis(
+                edge_x_insts, axis, edges, iou_threshold, ids_on_edge_x
             )
             # Merge not on edge with merged edge instances
+            ids.extend(merged_ids)
             roi_coords = [
                 roi for i, roi in zip(is_on_edge_x, roi_coords) if not i
             ]
@@ -88,24 +132,19 @@ def merge_masks_at_splitponits(
         for edge in edges_y:
             edges = np.arange(edge - max_offset, edge + max_offset + 1)
             is_on_edge_y = [is_on_edge(roi, edges, axis) for roi in roi_coords]
-            edge_y_insts = [
-                roi for i, roi in zip(is_on_edge_y, roi_coords) if i
-            ]
-            edge_y_merged = merge_over_axis(
-                edge_y_insts, axis, edges, iou_threshold
+            ids_on_edge_y = [id_x for i, id_x in zip(is_on_edge_y, ids) if i]
+            ids = [id_x for i, id_x in zip(is_on_edge_y, ids) if not i]
+            edge_y_insts = [ro for i, ro in zip(is_on_edge_y, roi_coords) if i]
+            edge_y_merged, merged_ids = merge_over_axis(
+                edge_y_insts, axis, edges, iou_threshold, ids_on_edge_y
             )
             # Merge not on edge with merged edge instances
+            ids.extend(merged_ids)
             roi_coords = [
                 roi for i, roi in zip(is_on_edge_y, roi_coords) if not i
             ]
             roi_coords.extend(edge_y_merged)
-
-    return roi_coords
-
-
-def is_on_edge(roi_coord: np.ndarray, edges_x: list, axis: int) -> bool:
-    """Check if the roi_coord is on the x edge of the patch."""
-    return np.any(roi_coord[:, :, 1 - axis] == edges_x)
+    return roi_coords, ids
 
 
 def merge_over_axis(
@@ -113,17 +152,27 @@ def merge_over_axis(
     axis: int,
     edges: np.ndarray,
     threshold: float = 0.85,
-) -> list[np.ndarray]:
+    ids: Optional[list[list[int]]] = None,
+) -> tuple[list[np.ndarray], list[list]]:
     """Merge instances over axis."""
-    for i, inst in enumerate(instances):
-        for j, inst_ref in enumerate(instances[i + 1 :]):
-            if is_one_object(
-                inst, inst_ref, edges=edges, threshold=threshold, axis=axis
-            ):
+    if not ids:
+        ids = [[i] for i in range(len(instances))]
+
+    for i, (id_i, inst) in enumerate(zip(ids, instances)):
+        for j, (id_j, inst_ref) in enumerate(
+            zip(ids[i + 1 :], instances[i + 1 :])
+        ):
+            if is_one_object(inst, inst_ref, edges, threshold, axis=axis):
                 instances[i] = merge_two_contours(inst, inst_ref)
-                instances.pop(i + j + 1)
+                ids[i] = id_i + id_j
+                instances.pop(i + j + 1), ids.pop(i + j + 1)
                 j -= 1
-    return instances
+    return instances, ids
+
+
+def is_on_edge(roi_coord: np.ndarray, edges_x: list, axis: int) -> bool:
+    """Check if the roi_coord is on the x edge of the patch."""
+    return np.any(roi_coord[:, :, 1 - axis] == edges_x)
 
 
 def merge_two_contours(cont1: np.ndarray, cont2: np.ndarray) -> np.ndarray:
--- myo_sam/inference/utils.py
+++ myo_sam/inference/utils.py
@@ -1,4 +1,3 @@
-import numpy as np
 import hashlib
 
 
@@ -6,42 +5,3 @@ def hash_bytes(bb: bytes) -> str:
     """Hash a numpy array for caching."""
     hash = hashlib.sha256(bb)
     return hash.hexdigest()
-
-
-def split_image_into_patches(
-    image: np.ndarray, patch_size: tuple[int, int]
-) -> tuple[tuple[int, int], list[np.ndarray]]:
-    """Split an image into patches of a given size."""
-    height, width = image.shape[:2]
-    patch_height, patch_width = patch_size
-
-    if patch_height > height or patch_width > width:
-        return (0, 0), [image]
-
-    patches = []
-    width_reminder = width % patch_width
-    height_reminder = height % patch_height
-    width_range = [*range(0, width, patch_width)]
-    height_range = [*range(0, height, patch_height)]
-
-    # if reminder less than quarter of patch-size, merge it to the last patch
-    if width_reminder < patch_size[0] / 4:
-        width_range[-1] += width_reminder
-    else:
-        width_range.append(width)
-    # if reminder less than quarter of patch-size, merge it to the last patch
-    if height_reminder < patch_size[1] / 4:
-        height_range[-1] += height_reminder
-    else:
-        height_range.append(height)
-
-    # grid is needed for later reconstruction
-    grid = (len(height_range) - 1, len(width_range) - 1)
-
-    for i in range(len(height_range) - 1):
-        for j in range(len(width_range) - 1):
-            left, right = width_range[j], width_range[j + 1]
-            lower, upper = height_range[i], height_range[i + 1]
-            patch = image[lower:upper, left:right, ...]
-            patches.append(patch)
-    return grid, patches
