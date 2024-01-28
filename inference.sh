1b80920ac1e7d7dffbc571fec6edb220768cddf7
Noza23 <G.Nozadze@campus.lmu.de>
2024-01-28 20:32:34 +0100

    .sh scripts for starting train, inf

--- inference.sh
+++ inference.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#
 #SBATCH --partition= # set partition name
 #SBATCH -w  # set node name
 #SBATCH --gpus= # set number of GPUs
@@ -9,11 +8,9 @@
 #SBATCH -o logs/multi-gpu-training_%A_%a.out
 #SBATCH -e logs/multi-gpu-training_%A_%a.err
 #SBATCH -A # set account name
-#SBATCH --array=0-15%1 # Array job
 
-TORCH_DISTRIBUTED_DEBUG=INFO
 
 singularity exec --pwd $(pwd) --nv \
   -B /myovision:/mnt \
   image \
-  bash -c "cd /mnt/myovision-sam && torchrun --standalone --nproc_per_node=gpu train.py"
+  bash -c "cd /mnt/myovision-sam && python3 inference.py"
